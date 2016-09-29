307858489244946060b2487d62cf2de397899675
Luis Romero <oremorsiul15@hotmail.com>
2016-09-29 00:24:18 -0400

    Add project files.

---
+++ encriptions/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace encriptions
+{
+    partial class encriptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.keyTb = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputTb = new System.Windows.Forms.TextBox();
+            this.outputTb = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.keyTb);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 220);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 121);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "settings";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "key";
+            // 
+            // keyTb
+            // 
+            this.keyTb.Location = new System.Drawing.Point(45, 29);
+            this.keyTb.Name = "keyTb";
+            this.keyTb.Size = new System.Drawing.Size(92, 20);
+            this.keyTb.TabIndex = 3;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(99, 62);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "decode";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 62);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "encript";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "output";
+            // 
+            // inputTb
+            // 
+            this.inputTb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.inputTb.Location = new System.Drawing.Point(12, 29);
+            this.inputTb.Multiline = true;
+            this.inputTb.Name = "inputTb";
+            this.inputTb.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTb.Size = new System.Drawing.Size(237, 185);
+            this.inputTb.TabIndex = 4;
+            // 
+            // outputTb
+            // 
+            this.outputTb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTb.Location = new System.Drawing.Point(266, 29);
+            this.outputTb.Multiline = true;
+            this.outputTb.Name = "outputTb";
+            this.outputTb.ReadOnly = true;
+            this.outputTb.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTb.Size = new System.Drawing.Size(241, 312);
+            this.outputTb.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 347);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(495, 17);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // encriptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 369);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.outputTb);
+            this.Controls.Add(this.inputTb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "encriptions";
+            this.Text = "encription";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputTb;
+        private System.Windows.Forms.TextBox outputTb;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox keyTb;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ encriptions/Form1.cs
@@ -0,0 +1,544 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace encriptions
+{
+    //arreglar 5 valores similares
+    public partial class encriptions : Form
+    {
+        
+        // set to code or decode mode
+        bool code = false;
+        // password key
+        int key = 0;
+        // input in a char array
+        char[] input;
+        //functions var
+        int valueof = 0;
+        char valueof1 = ' ';
+
+
+        public encriptions()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //mode coding
+            code = true;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //mode decode
+            code = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            progressBar1.Value = 0;
+            progressBar1.Maximum = inputTb.Text.Length;
+            outputTb.Text = "";
+            //getting key textbox value
+            try
+            {
+                key = Convert.ToInt32(keyTb.Text);
+                input = (inputTb.Text).ToCharArray();
+                string keytest = (key * 37).ToString();
+                //checking that key don't exeed 8 digits
+                if ( keytest.Length >= 8 || key == 0)
+                {
+                    MessageBox.Show("Invalid Key");
+                    keyTb.Text = "";
+                    return;
+                }
+            }
+            catch
+            {
+                //error handler
+                MessageBox.Show("unable to get input or key");
+                return;
+            }
+            if (code == true)
+            {
+                string first = "";
+                string second = "";
+                for (int i = 0; i < inputTb.Text.Length; i++)
+                {
+                  
+                    first = (InputValue(input[i])*key).ToString("00000000");
+                    second = second + first;
+                    progressBar1.Value++;
+                }
+                outputTb.Text = second;
+            }
+            else if (code == false)
+            {
+                Decode();
+            }
+            else
+            {
+                
+            }
+        }
+
+        private int InputValue(char charval)
+        {
+
+            switch (charval)
+            {
+                case 'a':
+                    {
+                        valueof = 1;
+                        break;
+                    }
+                case 'b':
+                    {
+                        valueof = 2;
+                        break;
+                    }
+                case 'c':
+                    {
+                        valueof = 3;
+                        break;
+                    }
+                case 'd':
+                    {
+                        valueof = 4;
+                        break;
+                    }
+                case 'e':
+                    {
+                        valueof = 5;
+                        break;
+                    }
+                case 'f':
+                    {
+                        valueof = 6;
+                        break;
+                    }
+                case 'g':
+                    {
+                        valueof = 7;
+                        break;
+                    }
+                case 'h':
+                    {
+                        valueof = 8;
+                        break;
+                    }
+                case 'i':
+                    {
+                        valueof = 9;
+                        break;
+                    }
+                case 'j':
+                    {
+                        valueof = 10;
+                        break;
+                    }
+                case 'k':
+                    {
+                        valueof = 11;
+                        break;
+                    }
+                case 'l':
+                    {
+                        valueof = 12;
+                        break;
+                    }
+                case 'm':
+                    {
+                        valueof = 13;
+                        break;
+                    }
+                case 'n':
+                    {
+                        valueof = 14;
+                        break;
+                    }
+                case 'o':
+                    {
+                        valueof = 15;
+                        break;
+                    }
+                case 'p':
+                    {
+                        valueof = 16;
+                        break;
+                    }
+                case 'q':
+                    {
+                        valueof = 17;
+                        break;
+                    }
+                case 'r':
+                    {
+                        valueof = 18;
+                        break;
+                    }
+                case 's':
+                    {
+                        valueof = 19;
+                        break;
+                    }
+                case 't':
+                    {
+                        valueof = 20;
+                        break;
+                    }
+                case 'u':
+                    {
+                        valueof = 21;
+                        break;
+                    }
+                case 'v':
+                    {
+                        valueof = 22;
+                        break;
+                    }
+                case 'w':
+                    {
+                        valueof = 23;
+                        break;
+                    }
+                case 'x':
+                    {
+                        valueof = 24;
+                        break;
+                    }
+                case 'y':
+                    {
+                        valueof = 25;
+                        break;
+                    }
+                case 'z':
+                    {
+                        valueof = 26;
+                        break;
+                    }
+                case '.':
+                    {
+                        valueof = 27;
+                        break;
+                    }
+                case ',':
+                    {
+                        valueof = 28;
+                        break;
+                    }
+                case '0':
+                    {
+                        valueof = 29;
+                        break;
+                    }
+                case '1':
+                    {
+                        valueof = 30;
+                        break;
+                    }
+                case '2':
+                    {
+                        valueof = 31;
+                        break;
+                    }
+                case '3':
+                    {
+                        valueof = 32;
+                        break;
+                    }
+                case '4':
+                    {
+                        valueof = 33;
+                        break;
+                    }
+                case '5':
+                    {
+                        valueof = 27;
+                        break;
+                    }
+                case '6':
+                    {
+                        valueof = 34;
+                        break;
+                    }
+                case '7':
+                    {
+                        valueof = 35;
+                        break;
+                    }
+                case '8':
+                    {
+                        valueof = 36;
+                        break;
+                    }
+                case '9':
+                    {
+                        valueof = 37;
+                        break;
+                    }
+                default:
+                    {
+                        valueof = 0;
+                        break;
+                    }
+
+
+
+            }
+            return valueof;
+        }
+
+        private char outputValue(int charval)
+        {
+
+            switch (charval)
+            {
+                case 1:
+                    {
+                        valueof1 = 'a';
+                        break;
+                    }
+                case 2:
+                    {
+                        valueof1 = 'b';
+                        break;
+                    }
+                case 3:
+                    {
+                        valueof1 = 'c' ;
+                        break;
+                    }
+                case 4:
+                    {
+                        valueof1 = 'd';
+                        break;
+                    }
+                case 5:
+                    {
+                        valueof1 = 'e';
+                        break;
+                    }
+                case 6:
+                    {
+                        valueof1 = 'f' ;
+                        break;
+                    }
+                case 7:
+                    {
+                        valueof1 = 'g';
+                        break;
+                    }
+                case 8:
+                    {
+                        valueof1 = 'h';
+                        break;
+                    }
+                case 9:
+                    {
+                        valueof1 = 'i';
+                        break;
+                    }
+                case 10:
+                    {
+                        valueof1 = 'j';
+                        break;
+                    }
+                case 11:
+                    {
+                        valueof1 = 'k';
+                        break;
+                    }
+                case 12:
+                    {
+                        valueof1 = 'l';
+                        break;
+                    }
+                case 13:
+                    {
+                        valueof1 = 'm';
+                        break;
+                    }
+                case 14:
+                    {
+                        valueof1 = 'n';
+                        break;
+                    }
+                case 15:
+                    {
+                        valueof1 = 'o';
+                        break;
+                    }
+                case 16:
+                    {
+                        valueof1 = 'p';
+                        break;
+                    }
+                case 17:
+                    {
+                        valueof1 = 'q';
+                        break;
+                    }
+                case 18:
+                    {
+                        valueof1 = 'r';
+                        break;
+                    }
+                case 19:
+                    {
+                        valueof1 = 's';
+                        break;
+                    }
+                case 20:
+                    {
+                        valueof1 = 't';
+                        break;
+                    }
+                case 21:
+                    {
+                        valueof1 = 'u';
+                        break;
+                    }
+                case 22:
+                    {
+                        valueof1 = 'v';
+                        break;
+                    }
+                case 23:
+                    {
+                        valueof1 = 'w';
+                        break;
+                    }
+                case 24:
+                    {
+                        valueof1 = 'x';
+                        break;
+                    }
+                case 25:
+                    {
+                        valueof1 = 'y';
+                        break;
+                    }
+                case 26:
+                    {
+                        valueof1 = 'z';
+                        break;
+                    }
+                case 27:
+                    {
+                        valueof1 = '.';
+                        break;
+                    }
+                case 28:
+                    {
+                        valueof1 = ',';
+                        break;
+                    }
+                case 29:
+                    {
+                        valueof1 = '0';
+                        break;
+                    }
+                case 30:
+                    {
+                        valueof1 = '1';
+                        break;
+                    }
+                case 31:
+                    {
+                        valueof1 = '2';
+                        break;
+                    }
+                case 32:
+                    {
+                        valueof1 = '3';
+                        break;
+                    }
+                case 33:
+                    {
+                        valueof1 = '4';
+                        break;
+                    }
+                case 34:
+                    {
+                        valueof1 = '5';
+                        break;
+                    }
+                case 35:
+                    {
+                        valueof1 = '6';
+                        break;
+                    }
+                case 36:
+                    {
+                        valueof1 = '7';
+                        break;
+                    }
+                case 37:
+                    {
+                        valueof1 = '8';
+                        break;
+                    }
+                case 38:
+                    {
+                        valueof1 = '9';
+                        break;
+                    }
+                default:
+                    {
+                        valueof1 = ' ';
+                        break;
+                    }
+
+
+
+            }
+            return valueof1;
+        }
+        private void Decode()
+        {
+            string first = "";
+            int third = 0;
+            string f4 = "";
+            int hold = 0;
+            for (int j = 0; j < inputTb.Text.Length/8;j++)
+            {
+                first = input[j*hold + 0].ToString() + input[j * hold + 1].ToString() + input[j * hold + 2].ToString() + input[j * hold + 3].ToString() + input[j * hold + 4].ToString() + input[j * hold + 5].ToString() + input[j * hold + 6].ToString() + input[j * hold + 7].ToString();
+                hold = 8;
+                try
+                {
+                    third = Convert.ToInt32(first);
+                 }
+                  catch
+                  {
+                      MessageBox.Show("not a decodeable input");
+                      return;
+                  }
+                progressBar1.Value+= 8;
+                f4 = f4 + outputValue(third/key).ToString();
+            }
+            outputTb.Text = f4;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            inputTb.Clear();
+            outputTb.Clear();
+            keyTb.Clear();
+            
+        }
+    }
+}
